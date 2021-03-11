041e462ff69465a27e55774a45908f2e85b66f4b
rhmtazad <rhmtazad@gmail.com>
2021-03-11 20:27:51 +0430

    Added more descriptions about the version and the API

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
 from setuptools import setup
 
 classifiers = [
-  'Development Status :: 5 - Production/Stable',
-  'Intended Audience :: Education',
-  'Operating System :: Microsoft :: Windows :: Windows 10',
-  'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3'
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
 ]
+
+with open("README.txt", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='hydradb',
-    version='2.0',
+    version='2.1',
     packages=['hydra'],
     url='https://github.com/rhmtazad/hydradb',
     license='MIT License',
     classifiers=classifiers,
     author='Rahmat Azad',
     author_email='rhmtazad@gmail.com',
-    description='Manage SQLite operations much faster and easier using Python',
+    description='Manage SQLite/Python operations much faster and easier using this API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=['']
 )
